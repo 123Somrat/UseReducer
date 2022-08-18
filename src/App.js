3c07f924c16870e61c43a5a483cc6b0cbce86f95
somrat <jafaruddin875@gmail.com>
2022-08-18 11:43:07 +0200

    simple increment decrement reset functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+   const [count,setCount] = useState(0)
+
+   const Increment = (num)=>{
+       setCount((prev)=>prev+num)
+      }
+   const Decrement = (num)=>{
+    setCount((prev)=>prev-num)
+     }
+
+   const Reset = ()=>{
+         setCount(0)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <h1>Count:{count}</h1> 
+         <button onClick={()=>Increment(15)}>Increment</button>
+         <button onClick={()=>Decrement(10)}>Decrement</button>
+         <button onClick={Reset}>Reset</button>
     </div>
   );
 }
