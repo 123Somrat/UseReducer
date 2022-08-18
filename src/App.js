fc088a5a2b003ce9dbe1672907a14b127790f00c
somrat <jafaruddin875@gmail.com>
2022-08-18 12:01:20 +0200

    Increment count useing Reducer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import { useState} from "react";
+
 import { useReducer } from "react";
 
 function App() {
-   const [count,setCount] = useState(0)
+   
  // useing useReducer hooks
   // useReducer hooks takes two parameter one is function and second one is initialState
   // here Reducer is a function and initialState is initial value of our state
   // useReducer hooks returns a array
    // here useing state we can update our state value
   // here dispatch is a function
- const  [state,dispatch] = useReducer(reducer,initialState)
+ const  [state,dispatch] = useReducer(reducer,0)
 
 
+ // Creating reducer function
+
+ function reducer(state,action){
+    if(action.type==="increment"){
+      return state + 1
+    }
+ }
 
    const Increment = (num)=>{
-       setCount((prev)=>prev+num)
+       dispatch({type:"increment"})
       }
-   const Decrement = (num)=>{
-    setCount((prev)=>prev-num)
-     }
-
-   const Reset = ()=>{
-         setCount(0)
-    }
+   
   return (
     <div className="App">
-         <h1>Count:{count}</h1> 
-         <button onClick={()=>Increment(15)}>Increment</button>
-         <button onClick={()=>Decrement(10)}>Decrement</button>
-         <button onClick={Reset}>Reset</button>
+         <h1>Count:{state}</h1> 
+         <button onClick={Increment}>Increment</button>
+         <button >Decrement</button>
+         <button >Reset</button>
     </div>
   );
 }
